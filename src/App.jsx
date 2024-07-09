6da978f3985efc7ee7429716548760e8911a861d
abc <abhaygupta.hsj@gmail.com>
2024-07-09 21:16:43 +0530

    I guess the project is cursed as it always ends with an error!

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Home, About, Projects, Contact } from "./pages";
+import Navbar from "./components/Navbar";
 
-import React from 'react'
-
-import {Home, About, Projects, Contact} from './pages';
+import React from "react";
 
 const app = () => {
   return (
-    <main className='bg-slate-300/20'>
+    <main className="bg-slate-300/20">
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-          <Route path='/contact' element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
-  )
-}
+  );
+};
 
-export default app
+export default app;
