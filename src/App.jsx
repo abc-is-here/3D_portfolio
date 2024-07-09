f6a27e1a1c8235b36bc33892fb307b296131cbe0
abc <abhaygupta.hsj@gmail.com>
2024-07-09 15:18:16 +0530

    I am making a 3D portfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import React from 'react'
+
+import {Home, About, Projects, Contact} from './pages';
+
+const app = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </Router>
+    </main>
+  )
+}
+
+export default app
